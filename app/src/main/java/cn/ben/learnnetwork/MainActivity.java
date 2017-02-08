82b4a2bed7bbf2371f1cd3ea871f7305f053c8ab
dqshare <739362790@qq.com>
2017-02-08 20:35:59 +0800

    Volley

--- app/src/main/java/cn/ben/learnnetwork/MainActivity.java
+++ app/src/main/java/cn/ben/learnnetwork/MainActivity.java
@@ -1,8 +1,21 @@
 package cn.ben.learnnetwork;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.ImageView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.NetworkImageView;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -19,6 +32,7 @@ import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.params.HttpParams;
 import org.apache.http.params.HttpProtocolParams;
 import org.apache.http.protocol.HTTP;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -26,9 +40,15 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+@SuppressWarnings("FieldCanBeLocal")
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView iv_image;
+    private NetworkImageView nv_image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +64,77 @@ public class MainActivity extends AppCompatActivity {
                 useHttpUrlConnectionPost("http://www.baidu.com");
             }
         }).start();
+
+        RequestQueue mQueue = Volley.newRequestQueue(getApplicationContext());
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, "https://m.baidu.com/?from=844b&vit=fps", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.i("he", response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("he", error.getMessage(), error);
+            }
+        });
+        mQueue.add(stringRequest);
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, "http://apistore.baidu.com/microservice/weather", new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.i("he", response.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("he", error.getMessage(), error);
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> map = super.getParams();
+                map.put("citypinyin", "beijing");
+                return map;
+            }
+        };
+        mQueue.add(jsonObjectRequest);
+
+        iv_image = (ImageView) findViewById(R.id.iv_image);
+//        ImageRequest imageRequest = new ImageRequest(
+//                "http://img.my.csdn.net/uploads/201603/26/1458988468_5804.jpg",
+//                new Response.Listener<Bitmap>() {
+//                    @Override
+//                    public void onResponse(Bitmap response) {
+//                        iv_image.setImageBitmap(response);
+//                    }
+//                }, 0, 0, Bitmap.Config.RGB_565, new Response.ErrorListener() {
+//            @Override
+//            public void onErrorResponse(VolleyError error) {
+//                iv_image.setImageResource(android.R.drawable.ic_btn_speak_now);
+//            }
+//        });
+//        mQueue.add(imageRequest);
+
+        ImageLoader imageLoader = new ImageLoader(mQueue, new ImageLoader.ImageCache() {
+            final HashMap<String, Bitmap> map = new HashMap<>();
+
+            @Override
+            public Bitmap getBitmap(String url) {
+                return map.get(url);
+            }
+
+            @Override
+            public void putBitmap(String url, Bitmap bitmap) {
+                map.put(url, bitmap);
+            }
+        });
+        ImageLoader.ImageListener listener = ImageLoader.getImageListener(iv_image, android.R.drawable.ic_btn_speak_now, android.R.drawable.ic_delete);
+        imageLoader.get("http://img.my.csdn.net/uploads/201603/26/1458988468_5804.jpg", listener);
+        
+        nv_image = (NetworkImageView) findViewById(R.id.nv_image);
+        nv_image.setDefaultImageResId(android.R.drawable.ic_menu_report_image);
+        nv_image.setErrorImageResId(android.R.drawable.stat_notify_error);
+        nv_image.setImageUrl("http://img.my.csdn.net/uploads/201603/26/1458988468_5804.jpg", imageLoader);
     }
 
     private HttpClient createHttpClient() {
